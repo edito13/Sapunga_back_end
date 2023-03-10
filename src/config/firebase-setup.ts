753f1ac71662d776e20d3cf6e49bbaf92f62f035
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-03-10 17:15:29 +0100

    refactor: put all code to save a file into FirebaseUploadService class

--- src/config/firebase-setup.ts
+++ src/config/firebase-setup.ts
@@ -1,42 +1,10 @@
 import firebaseConfig from "./firebase.json";
 import { initializeApp } from "firebase/app";
-import {
-  ref as StorageRef,
-  UploadMetadata,
-  getStorage,
-} from "firebase/storage";
-import { Request } from "express";
-import path from "path";
-import { v4 } from "uuid";
+import { getStorage } from "firebase/storage";
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const storage = getStorage(firebaseApp);
 // "gs://react-login-10ba2.appspot.com"
 
-function uploadFileToFirebase(req: Request) {
-  if (!req.file) throw new Error("No file found.");
-
-  const extname = path.extname(req.file.originalname);
-  const filename = `${v4()}.${extname}`;
-
-  const storageRef = StorageRef(storage);
-  const metadata: UploadMetadata = {
-    contentType: req.file.mimetype,
-  };
-
-  // const snapshot = await uploadBytesResumable(
-  //   storageRef,
-  //   req.file.buffer,
-  //   metadata
-  // );
-
-  // uploadBytes(storageRef, req.file.buffer).then((snapshot) => {
-  //   console.log("Uploaded a blob or file!");
-  // });
-
-  // const downloadedURL = await getDownloadURL(snapshot.ref);
-  return { storage, storageRef };
-}
-
-export { firebaseApp, storage, uploadFileToFirebase };
+export { firebaseApp, storage };
--- src/services/FirebaseUploadService.ts
+++ src/services/FirebaseUploadService.ts
@@ -1,17 +1,43 @@
 import { Request, Response, NextFunction } from "express";
-import "../config/firebase-setup";
-import { uploadFileToFirebase } from "../config/firebase-setup";
+import path from "path";
+import {
+  ref as StorageRef,
+  UploadMetadata,
+  uploadBytesResumable,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 import { UploadProvider } from "./interfaces/UploadProvider";
+import { v4 } from "uuid";
+import { storage } from "../config/firebase-setup";
 
 export class FirebaseUploadService implements UploadProvider {
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
-      const { storage, storageRef } = uploadFileToFirebase(req);
+      if (!req.file) throw new Error("No file found.");
+
+      const extname = path.extname(req.file.originalname);
+      const filename = `${v4()}.${extname}`;
+
+      const storageRef = StorageRef(storage, `uploads/${filename}`);
+      const metadata: UploadMetadata = {
+        contentType: req.file.mimetype,
+      };
+
+      const snapshot = await uploadBytesResumable(
+        storageRef,
+        req.file.buffer,
+        metadata
+      );
+
+      uploadBytes(storageRef, req.file.buffer).then((snapshot) => {
+        console.log("Uploaded a blob or file!");
+      });
+
+      const downloadedURL = await getDownloadURL(snapshot.ref);
 
       return res.send({
-        url: "downloadedURL!",
-        storage,
-        storageRef,
+        downloadedURL,
       });
     } catch (e: any) {
       return res.status(400).send({ status: "error", message: e.message });
