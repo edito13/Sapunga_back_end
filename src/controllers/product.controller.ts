0121df1ce214e8a912a88a3f7ffbf8fd78a4ef36
edito13 <editotchocosso13@gmail.com>
2023-03-29 17:48:36 +0100

    Fixing details on Back-End

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -11,11 +11,11 @@ router.post("/regist", async (req, res) => {
     if (!name) throw "Nome não enviado";
     else if (!describe) throw "Descrição não enviada";
     else if (!price) throw "Preço não enviado";
-    else if (!categoryID) throw 'Categoria não especificada'
+    else if (!categoryID) throw "Categoria não especificada";
 
-    const category = await Category.findById(categoryID)
+    const category = await Category.findById(categoryID);
 
-    if(!category) throw 'Categoria não existe'
+    if (!category) throw "Categoria não existe";
 
     const product = await Product.create({
       urlPhoto,
@@ -25,9 +25,9 @@ router.post("/regist", async (req, res) => {
       category: categoryID,
     });
 
-    if(!product) throw 'Não foi possível cadastrar o produto'
+    if (!product) throw "Não foi possível cadastrar o produto";
 
-    const products = await Product.find({}).populate('category')
+    const products = await Product.find({}).populate("category");
     res.status(201).json(products);
   } catch (error) {
     res.status(405).send({ error });
@@ -36,7 +36,7 @@ router.post("/regist", async (req, res) => {
 
 router.get("/selectAll", async (req, res) => {
   try {
-    const products = await Product.find({}).populate('category');
+    const products = await Product.find({}).populate("category");
     res.json(products);
   } catch (error) {
     res.status(404).send({ error });
@@ -47,7 +47,7 @@ router.get("/selectOne/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const products = await Product.findById(id).populate('category');
+    const products = await Product.findById(id).populate("category");
     res.json(products);
   } catch (error) {
     res.status(404).send({ error });
@@ -65,6 +65,7 @@ router.delete("/delete", async (req, res) => {
     res.json(product);
   } catch (error) {
     res.status(400).send({ error });
-}});
+  }
+});
 
 module.exports = (app: any) => app.use("/product", router);
