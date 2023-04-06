7af5bf982b41a6f132f7fbd081e4cf3146f4c85a
edito13 <editotchocosso13@gmail.com>
2023-04-06 05:58:01 +0100

    Admin's model was created succefully

---
+++ src/models/admin.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  code: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Admin = model("Admin", AdminSchema);
+
+export default Admin;
